1d2d2e5ddf231cdb1b1664c09e9e0aec8076beb3
IAmSalvaMartini <starraos@gmail.com>
2021-10-31 00:26:37 +0530

    Proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,9 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_VERSION := 4.9
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/gcc/host/linux-x86/eva-gcc/bin/aarch64-elf-
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
