3bd6f303fff2405b72f6e66567f5651edb4026c3
clarencelol <clarencekuiek@icloud.com>
2021-10-30 18:58:58 +0530

    vince: properties: Set SOC manufacturer and chipset properties
    Signed-off-by: IAmSalvaMartini <starraos@gmail.com>

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Soc
+PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Qualcomm
+PRODUCT_VENDOR_PROPERTIES += ro.soc.model=SD625
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
