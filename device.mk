72e35068cc54e0c33e6b983599fbede36ee4e33c
ChihYao Chien <ccchien@google.com>
2021-10-30 19:36:33 +0530

    vince: Add qti-telephony-hidl-wrapper-prd Provide qti-telephony-hidl-wrapper-prd for modules in product partition to include.

--- device.mk
+++ device.mk
@@ -414,7 +414,9 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
+    telephony-ext    
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper_prd.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
