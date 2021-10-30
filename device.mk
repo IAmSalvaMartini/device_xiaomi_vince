c962dd8052d067a6b549f42301bbce15aefe023a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-30 19:39:17 +0530

    vince: Uprev bluetooth audio HIDL impl to 2.1

--- device.mk
+++ device.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
